82d3874a1ff4c82b1dae72fd7901b8f3204de18f
KCCyclone <caedon.newton@gmail.com>
2023-11-05 15:07:01 -0600

    Saved Statcast Data

--- PitchPrediction.py
+++ PitchPrediction.py
@@ -1,8 +1,7 @@
-pbp = 0.4022 #pitcher-batter prior
-pcp = 0.2389 #pitcher-count prior
-ppv = 0.1529 #previous pitch's velocity
-ppt = 0.1138 #previous pitch type
-i = 0.065 #inning
-o = 0.0522 #outs
-sd = 0.0408 #score difference
-bo = 0.0398 #bases occupied
+import pandas as pd
+
+from pybaseball import statcast
+#stats = statcast(start_dt="2022-06-24", end_dt="2022-06-28")
+stats = pd.read_pickle('stats.pickle')
+print(stats.columns)
+#stats.to_pickle('stats.pickle')
