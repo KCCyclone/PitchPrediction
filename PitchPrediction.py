4756e1f998e600c7674a7c2211cea19f85fb5945
KCCyclone <caedon.newton@gmail.com>
2023-11-05 16:33:48 -0600

    Graph

--- PitchPrediction.py
+++ PitchPrediction.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn import svm
 from pybaseball import statcast
+import numpy as np
+import matplotlib.pyplot as plt
 
 #stats = statcast(start_dt="2022-06-24", end_dt="2022-06-28")
 stats = pd.read_pickle('stats.pickle')
@@ -18,3 +20,14 @@ clf.fit(independent_vars_training, dependent_var_training)
 dependent_var_predicted = clf.predict(independent_vars_test)
 print(list(dependent_var_test))
 print(dependent_var_predicted)
+# Generate some sample data
+x = independent_vars_test['release_pos_x']
+y = independent_vars_test['release_pos_y']
+pitch_to_num = {v:i for i, v in enumerate(set(dependent_var_predicted))}
+classes = [pitch_to_num[v] for v in dependent_var_predicted]
+# Create the scatter plot
+plt.scatter(x, y, c=classes)
+# Add labels and show the plot
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
