a169f48f574d6cdf6e38d5c8616f5f2a74066b07
KCCyclone <caedon.newton@gmail.com>
2023-11-05 14:20:30 -0600

    Initial Commit

---
+++ PitchPrediction.py
@@ -0,0 +1,8 @@
+pbp = 0.4022 #pitcher-batter prior
+pcp = 0.2389 #pitcher-count prior
+ppv = 0.1529 #previous pitch's velocity
+ppt = 0.1138 #previous pitch type
+i = 0.065 #inning
+o = 0.0522 #outs
+sd = 0.0408 #score difference
+bo = 0.0398 #bases occupied
