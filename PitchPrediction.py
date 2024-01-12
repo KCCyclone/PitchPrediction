8c81152b46518f90575e447fb66c28f1d380f0a5
KCCyclone <caedon.newton@gmail.com>
2024-01-12 16:46:56 -0600

    Graph showing percent chance it is to be a pitch type (Fastball, Offspeed, Breaking) for each count

--- PitchPrediction.py
+++ PitchPrediction.py
@@ -5,45 +5,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.calibration import CalibratedClassifierCV
 
+def categorize(pitch_type):
+    if pitch_type in ['FF', 'SI', 'FC']:
+        pitch_type = 'Fastball'
+    elif pitch_type in ['CH', 'FS', 'FO', 'SC']:
+        pitch_type = 'Offspeed'
+    elif pitch_type in ['CU', 'KC', 'CS', 'SL', 'ST', 'SV', 'KN', 'EP']:
+        pitch_type = 'Breaking'
+    else:
+        pitch_type = 'Other'
+    return pitch_type
 def predict_from_release_pos():
     #stats = statcast(start_dt="2022-06-24", end_dt="2022-06-28")
     stats = pd.read_pickle('stats.pickle')
     print(set(stats))
-    stats['fastball'] = stats['pitch_type'].map(lambda r: r in ['FF', 'SI', 'FC'])
+    stats['pitch_type_cat'] = stats['pitch_type'].map(categorize)
     x_divider = 0
     #y_divider = 54.2
-    stats = stats.loc[stats['release_pos_x']<=x_divider]
-    first_dep_col = 'release_pos_x'
+    #stats = stats.loc[stats['balls']<=x_divider]
+    test_group_size = 12
+    first_dep_col = 'balls'
     second_dep_col = 'strikes'
-    independent_col = 'fastball'
+    independent_col = 'pitch_type_cat'
     independent_vars = stats[[first_dep_col, second_dep_col]]
-    independent_vars_training = independent_vars.head(len(independent_vars)-100)
-    independent_vars_test = independent_vars.tail(100)
+    independent_vars_training = independent_vars.head(len(independent_vars)-test_group_size)
+    # independent_vars_test = independent_vars.tail(test_group_size)
+    independent_vars_test = pd.DataFrame(columns=[first_dep_col, second_dep_col])
+    for ball_count in range(4):
+        for strike_count in range(3):
+            independent_vars_test = pd.concat([independent_vars_test, 
+                                               pd.DataFrame([[ball_count, strike_count]], columns=independent_vars_test.columns)], 
+                                               ignore_index=True)
     dependent_var = stats[independent_col]
-    dependent_var_training = dependent_var.head(len(dependent_var)-100)
-    dependent_var_test = dependent_var.tail(100)
+    dependent_var_training = dependent_var.head(len(dependent_var)-test_group_size)
+    dependent_var_test = dependent_var.tail(test_group_size)
     print(independent_vars_training)
     print(f"{dependent_var_test=}")
     #stats.to_pickle('stats.pickle')
     clf = make_pipeline(StandardScaler(), svm.SVC(kernel = 'rbf'))
+    clf = CalibratedClassifierCV(clf)
     clf.fit(independent_vars_training, dependent_var_training)
     dependent_var_predicted = clf.predict(independent_vars_test)
+    dependent_var_probabilities = clf.predict_proba(independent_vars_test)
     print(list(dependent_var_test))
     print(dependent_var_predicted)
+    print(dependent_var_probabilities)
 
     # Generate some sample data
     x = independent_vars_test[first_dep_col]
     y = independent_vars_test[second_dep_col]
     
-     # Custom labels for True and False
-    custom_labels = {True: 'Fastball', False: 'Offspeed'}
+    # Custom labels for True and False
+    # custom_labeler = lambda b: "Fastball" if b else "Offspeed"
+    custom_labeler = lambda s: s
+
     
     display_vars = dependent_var_predicted
     # display_vars = dependent_var_test
 
      # Map predicted values to custom labels for better visualization
-    labels = [custom_labels[v] for v in display_vars]
+    labels = [custom_labeler(v) for v in display_vars]
     
     pitch_to_num = {v:i for i, v in enumerate(sorted(set(display_vars)))}
     classes = [pitch_to_num[v] for v in display_vars]
@@ -54,19 +77,23 @@ def predict_from_release_pos():
             to_display[p] = ([], [])
         to_display[p][0].append(xdp)
         to_display[p][1].append(ydp)
-
-    for p, (x, y) in to_display.items():
+    
+    for p, (xs, ys) in to_display.items():
         # Create the scatter plot
-        plt.scatter(x, y, label=p)
+        plt.scatter(xs, ys, label=p)
+    
+    for (_i, row), prob in zip(independent_vars_test.iterrows(), dependent_var_probabilities):
+        plt.text(row[first_dep_col], row[second_dep_col], f'{round(max(prob), 3):.1%}', fontsize=8, ha='left', va='bottom')
+    
     # Add labels and show the plot
-    plt.xlabel('Release Position X')
+    plt.xlabel('Balls')
     plt.ylabel('Strikes')
     plt.legend()
     # Custom legend labels
-    handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='blue', markersize=10, label=custom_labels[True]),
+    '''handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='blue', markersize=10, label=custom_labels[True]),
                plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='orange', markersize=10, label=custom_labels[False])]
     
-    plt.legend(handles=handles)
+    plt.legend(handles=handles)'''
     plt.show()
 
 predict_from_release_pos()
